1841560b36bccdb4a344be8a0396b63a8b4c9d4b
KhoiLe <mr.vjcspy@gmail.com>
2020-08-05 22:56:53 +0700

    - init bar module

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -1,15 +1,15 @@
 import foo from '@vjcspy/foo';
-import Bar from '@vjcspy/bar';
 import React from 'react';
-import {double} from '@vjcspy/web-ui';
+import { double } from '@vjcspy/web-ui';
+import { bar } from '@vjcspy/bar';
 
 export default function Home() {
   return (
-          <div>
-            Imported modules from another workspace:
-            <pre>{foo}</pre>
-            <pre>{double(2)}</pre>
-            <Bar/>
-          </div>
+    <div>
+      Imported modules from another workspace:
+      <pre>{foo}</pre>
+      <pre>{double(2)}</pre>
+      <pre>{bar()}</pre>
+    </div>
   );
 }
