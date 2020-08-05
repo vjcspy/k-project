2efac2b3a2c8b103d9214b8829059477cebd4038
KhoiLe <mr.vjcspy@gmail.com>
2020-08-05 22:48:42 +0700

    - init web module ui and hook

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -1,12 +1,15 @@
 import foo from '@vjcspy/foo';
 import Bar from '@vjcspy/bar';
+import React from 'react';
+import {double} from '@vjcspy/web-ui';
 
 export default function Home() {
   return (
-    <div>
-      Imported modules from another workspace:
-      <pre>{foo}</pre>
-      <Bar/>
-    </div>
+          <div>
+            Imported modules from another workspace:
+            <pre>{foo}</pre>
+            <pre>{double(2)}</pre>
+            <Bar/>
+          </div>
   );
 }
